de54d6bb0f3161d0d5f28f7b0bffc18c4079d02c
wers-apple <wers-apple@wers.local>
2019-05-13 07:44:07 +0800

    feat: express+sequelize 初始化

---
+++ config/db.js
@@ -0,0 +1,7 @@
+module.exports = {
+    host: '47.97.216.140',
+    database: 'test_db',
+    user: 'root',
+    password: 'F1n2g90',
+    dialect: 'mariadb'
+}
---
+++ sequelize.js
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize')
+const config = require('./config/db')
+
+//  创建sequelize实例
+const sequelize = new Sequelize(config.database, config.user, config.password, {
+    host: config.host,
+    dialect: config.dialect,//数据库类型(这里使用mariadb)
+    // timezon: '+08:00',
+    define: {
+        timestamps: false // 为模型添加 createdAt 和 updatedAt 两个时间戳字段（true or false）
+    },
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 3000,
+        idle: 10000
+    }
+})
+sequelize.authenticate()
+.then(()=>{
+    console.log('success')
+}).catch(err=>{
+    console.log(err)
+})
+module.exports = sequelize
