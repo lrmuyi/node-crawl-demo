87feed957312528e461d3d029b6768f340ecbaff
lrmuyi <wrsmile0101@163.com>
2019-11-08 11:34:07 +0800

    Update default.config.js

--- sys/server/db/config/default.config.js
+++ sys/server/db/config/default.config.js
@@ -1,8 +1,8 @@
 var config = {
   dialect: 'mariadb',
   database: 'node_crawl_test',
-  username: 'root',
-  password: 'F1n2g90',
+  username: '*****',
+  password: '*******',
   host: '47.97.216.140',
   port: 3306
 }
